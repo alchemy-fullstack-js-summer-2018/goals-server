90bd971bfe3b644acbd599efa27ce2f4937a4e92
Mario Quintana <pyrros2000@gmail.com>
2018-09-04 16:50:26 -0700

    Adding more basic files such models, routes, and testing files in case

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./lib/app');
 const connect = require('./lib/connect');
 
-connect(process.env.MONGODB_URI);
+connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/goals');
 
 const server = http.createServer(app);
 const port = process.env.port || 3000;
