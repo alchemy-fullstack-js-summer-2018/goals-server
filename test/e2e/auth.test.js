8b24c2d90689ae9f0d78dfd93f4c0bf54be80c26
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-05 13:27:04 -0700

    Adding request for auth e2e tests

---
+++ test/e2e/auth.test.js
@@ -0,0 +1,7 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+
+describe('Auth API', () => {
+    beforeEach(() => dropCollection('users'));
+});
