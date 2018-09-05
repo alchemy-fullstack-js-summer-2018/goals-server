e3b451feef9979cf3858bac3895e37a351de3cbf
MariahAdams <mariah.adams23@yahoo.com>
2018-09-05 08:29:14 -0700

    setting up auth files

---
+++ test/e2e/auth.test.js
@@ -0,0 +1,12 @@
+// const { assert } = require('chai');
+// const request = require('./request');
+const { dropCollection } = require('./_db');
+
+describe('Auth API', () => {
+
+    beforeEach(() => dropCollection('users'));
+    
+    it('testing', () => {
+
+    });
+});
