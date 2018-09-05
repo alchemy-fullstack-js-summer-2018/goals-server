5e9157726a0a3c6475da6a2d202bad4b4c727441
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-05 13:20:40 -0700

    Goal unit test complete

---
+++ test/unit/Goal.test.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+const Goal = require('../../lib/models/Goal');
+const { getErrors } = require('./helpers');
+
+describe('Goal model', () => {
+
+    it('Validates a good model', () => {
+        const data = {
+            name: 'Get better at coding!',
+            description: 'Practice, practice, practice',
+            completed: false,
+        };
+    
+        const goal = new Goal(data);
+    
+        data._id = goal._id;
+        assert.deepEqual(goal.toJSON(), data);
+
+    });
+
+    it('Validates required fields', () => {
+        const goal = new Goal({});
+        const errors = getErrors(goal.validateSync(), 4);
+        assert.equal(errors.name.kind, 'required'); 
+        assert.equal(errors.description.kind, 'required'); 
+        assert.equal(errors.completed.kind, 'required'); 
+        assert.equal(errors.userId.kind, 'required'); 
+    });
+});
