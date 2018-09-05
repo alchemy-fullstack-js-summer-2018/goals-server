7f812314d9161b624892df5661dad0141757a8e0
Antreo Pukay <acrosswalker@yahoo.com>
2018-09-05 12:03:54 -0700

    working on tests

--- test/e2e/goal.test.js
+++ test/e2e/goal.test.js
@@ -1,8 +1,9 @@
-const request = require('./request');
+const { request, save } = require('./request');
 const mongoose = require('mongoose');
 const assert = require('chai').assert;
 
-// const tokenService = require('../auth/token-service');
+const { dropCollection } = require('./db');
+const tokenService = require('../../lib/auth/token-service');
 
 const makeSimpleByID = (goal, user) => {
   const simple = {
@@ -20,22 +21,75 @@ const makeSimpleByID = (goal, user) => {
   return simple;
 };
 
-let goalOne;
-let goalTwo;
-// eslint-disable-next-line
-let tokenOne;
+let exercise;
+let photography;
 let userOne;
-let tokenTwo;
-// eslint-disable-next-line
+let tokenOne;
 let userTwo;
+let tokenTwo;
 
-const 
+const tonyRobbins = {
+  name: 'Tony Robbins',
+  email: 'robbins@rob.com',
+  password: 'abc123'
+};
 
-describe('Goals API', () => {
+const wayneDyer = {
+  name: 'Wayne Dyer',
+  email: 'wayne@dyer.com',
+  password: 'def456'
+};
+
+
+
+describe.skip('Goals API', () => {
   
   beforeEach(() => mongoose.connection.dropDatabase());
 
-  it('saves a goal', () => {
-    assert.isOk(goal._id);
+  beforeEach(() => dropCollection('goals'));
+  beforeEach(() => dropCollection('users'));
+
+  beforeEach(() => {
+    return request
+      .post('/api/auth/signup')
+      .send(tonyRobbins)
+      .then(({ body }) => {
+        tokenOne = body.token;
+        tokenService.verify(tokenOne)
+          .then(userBody => userOne = userBody);
+      });
   });
+
+  beforeEach(() => {
+    return request
+      .post('/api/auth/signup')
+      .send(wayneDyer)
+      .then(({ body }) => {
+        tokenTwo = body.token;
+        tokenService.verify(tokenTwo)
+          .then(userBody => userTwo = userBody);
+      });
+  });
+
+  beforeEach(() => {
+    return save('goals', {
+      subject: 'exercise',
+      goalList: ['run', 'swimming']
+    }, tokenOne)
+      .then(data => exercise  = data);
+  });
+  
+  beforeEach(() => {
+    return save('goals', {
+      subject: 'photography',
+      goalList: ['camera', 'film']
+    }, tokenOne)
+      .then(data => photography  = data);
+  });
+
+
+  it('saves a goal', () => {
+    assert.isOk(exercise._id);
+  });
+
 });
