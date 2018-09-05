377e167c7027e3e864c02d153066db68e113ada6
Mark Myers <markalope@gmail.com>
2018-09-05 10:36:28 -0700

    writing tests

---
+++ test/unit/Goals.test.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+const Goal = require('../../lib/models/Goal');
+const { getErrors } = require('./helpers');
+const { Types } = require('mongoose'); 
+
+describe('Goals model', () => {
+    it('is a valid good model', () => {
+        const data = {
+            userId: Types.ObjectId(),
+            name: 'Complete Code School. Get a Job.',
+            details: 'End of September is coming up fast. Time to start planning for the next steps.',
+            complete: false
+        };
+        const expected = {
+            data
+        };
+        const goal = new Goal(data);
+        
+        expected._id = goal._id;
+        assert.deepEqual(goal.toJSON(), expected);
+    });
+
+    it('checks required field', () => {
+        const goal = new Goal({});
+		
+        const errors = getErrors(goal.validateSync(), 2);
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.userId.kind, 'required');
+    });
+});
---
+++ test/unit/User.test.js
@@ -0,0 +1,36 @@
+const { assert } = require('chai');
+const User = require('../../lib/models/User');
+const { getErrors } = require('./helpers');
+
+describe('User model', () => {
+    const data = {
+        name: 'Mark-o-lope',
+        email: 'test@test.com'
+    };
+
+    const password = 'Abc123';
+
+    let user = null;
+    beforeEach(() => {
+        user = new User(data);
+        user.generateHash(password);
+    });
+
+    it('generates hash from password', () => {
+        assert.ok(user.hash);
+        assert.notEqual(user.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        assert.isOk(user.comparePassword(password));
+    });
+
+    it('checks required field', () => {
+        const user = new User({});
+    
+        const errors = getErrors(user.validateSync(), 3);
+        assert.equal(errors.name.kind, 'required');
+        assert.equal(errors.email.kind, 'required');
+        assert.equal(errors.hash.kind, 'required');
+    });
+});
