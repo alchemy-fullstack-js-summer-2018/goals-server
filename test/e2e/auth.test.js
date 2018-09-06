6d954c667471bdad790169fc323bb2a6db779cfd
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-06 10:55:11 -0700

    Signs up a user e2e test successful

--- test/e2e/auth.test.js
+++ test/e2e/auth.test.js
@@ -2,6 +2,25 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
 
-// describe('Auth API', () => {
-//     beforeEach(() => dropCollection('users'));
-// });
+describe('Auth API', () => {
+    beforeEach(() => dropCollection('users'));
+
+    let token;
+
+    beforeEach(() => {
+        return request
+            .post('/api/auth/signup')
+            .send({
+                name: 'Joe Schmoe',
+                email: 'joe@schmoe.com',
+                password: 'abc'
+            })
+            .then(({ body }) => {
+                token = body.token;
+            });
+    });
+
+    it('Signs up a user', () => {
+        assert.ok(token);
+    });
+});
