eefc0785ec96091383002fbe3f8f7dd62c5f6c3b
Rehmsy <sarahrehmer@gmail.com>
2018-09-05 17:47:07 -0700

    3 user unit tests passing

---
+++ test/unit/user.test.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const User = require('../../lib/models/user');
+const { Types } = require('mongoose');
+
+describe('User model', () => {
+
+  const data = {
+    name: 'Regina',
+    email: 'abc123@me.com'
+  };
+
+  const password = 'abc';
+
+  let user = null;
+  beforeEach(() => {
+    user = new User(data);
+    user.generateHash(password);
+  }); 
+    it('generates hash from password', () => {
+        assert.ok(user.hash);
+        assert.notEqual(user.hash, password);
+    });
+
+    it('compares password to hash', () => {
+        assert.isOk(user.comparePassword(password));
+    });
+});
