c8724ed97ad67e50eb881ab296da36ea5e6f2c25
Robyn Navarro <rnavarro182@gmail.com>
2018-09-06 11:38:48 -0700

    get users test passed

--- test/e2e/goals.test.js
+++ test/e2e/goals.test.js
@@ -9,6 +9,7 @@ describe('Goals API', () => {
 
     let token;
     let testGoal;
+    let testGoal2;
 
     beforeEach(() => {
         return createToken()
@@ -23,7 +24,16 @@ describe('Goals API', () => {
             .then(checkOk)
             .then(({ body }) => {
                 testGoal = body;
-                console.log('8888888********', testGoal);
+            });
+    });
+    beforeEach(() => {
+        return request
+            .post('/api/me/goals')
+            .set('Authorization', token)
+            .send({ goal: 'get it' })
+            .then(checkOk)
+            .then(({ body }) => {
+                testGoal2 = body;
             });
     });
 
@@ -31,5 +41,20 @@ describe('Goals API', () => {
         assert.equal(testGoal.goal, 'get good');
     });
 
-    
+    it('gets a goal', () => {
+        return request
+            .get('/api/me/goals')
+            .set('Authorization', token)
+            .then(checkOk)
+            .then(({ body }) => {
+                assert.deepEqual(body, [testGoal, testGoal2]);
+            });
+    });
+
+    // it('updates a goal', () => {
+    //     return request
+    //         .put(`api/me/goals/${testGoal._id}`)
+    //         .set('Authorization', token)
+    //         .then(checkOk)
+    // })
 });
--- test/e2e/users.test.js
+++ test/e2e/users.test.js
@@ -1,7 +1,44 @@
 const { assert } = require('chai');
 const request = require('../request');
-const { dropCollection } = require('../db');
+const { checkOk } = request;
+const { dropCollection, createToken } = require('../db');
 
 describe('Users API', () => {
 
+    beforeEach(() => dropCollection('users'));
+    beforeEach(() => dropCollection('goals'));
+
+    let token;
+    let testGoal;
+
+    beforeEach(() => {
+        return createToken()
+            .then(t => token = t);
+    });
+
+    beforeEach(() => {
+        return request
+            .post('/api/me/goals')
+            .set('Authorization', token)
+            .send({ goal: 'get good' })
+            .then(checkOk)
+            .then(({ body }) => {
+                testGoal = body;
+            });
+    });
+
+    it('gets users', () => {
+        return request
+            .get('/api/users')
+            .set('Authorization', token)
+            .then(checkOk)
+            .then(({ body }) => {
+                assert(body, [{ 
+                    _id: testGoal.author,
+                    completed: 0,
+                    total: 1,
+                    name: 'Mr. White' 
+                }]);
+            });
+    });
 });
