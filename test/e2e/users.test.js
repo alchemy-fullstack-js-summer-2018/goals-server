da874155a245d76fe0a2ac93630a2782f5e11eea
Easton Gorishek <ieaston26@gmail.com>
2018-09-07 10:58:20 -0700

    added route and aggregation to get all goals

---
+++ test/e2e/users.test.js
@@ -0,0 +1,44 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection, createToken } = require('./db');
+
+describe.only('Users API', () => {
+
+    const checkOk = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
+
+    beforeEach(() => dropCollection('users'));
+    beforeEach(() => dropCollection('goals'));
+
+    let token = '';
+    beforeEach(() => createToken().then(t => token = t));
+
+    function saveGoal(data) {
+        return request.post('/api/me/goals')
+            .set('Authorization', token)
+            .send(data)
+            .then(checkOk)
+            .then(({ body }) => {
+                const { _id, __v } = body;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+                return body;
+            });
+    }
+
+    let goal = { goal: 'Finish this lab' };
+    let anotherGoal = { goal: 'Get a job' };
+
+    beforeEach(() => saveGoal(goal).then(g => goal = g));
+    beforeEach(() => saveGoal(anotherGoal).then(g => anotherGoal = g));
+
+    it('Gets all goals per user', () => {
+        return request.get('/api/users')
+            .set('Authorization', token)
+            .then(({ body }) => {
+                console.log(body);
+            });
+    });
+});
